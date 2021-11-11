acce2e16ad02696b69716c1c91c1cddacff04653
anarysumbetov <74860324+anarysumbetov@users.noreply.github.com>
2021-11-11 22:07:13 +0600

    Styling pieces added

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -4,14 +4,31 @@ import "./Chessboard.css";
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
+interface Piece {
+    image: string;
+    x: number;
+    y: number;
+}
+
+const pieces: Piece[] = [];
+
+pieces.push({image: "assets/images/pawn_b.png", x: 0, y: 6})
+
 export default function Chessboard() {
     let board = [];
 
     for(let j = verticalAxis.length-1; j >= 0; j--) {
         for(let i = 0; i < horizontalAxis.length; i++) {
             const number = j + i + 2;
+            let image = undefined;
 
-            board.push(<Tile image="assets/images/pawn_b.png" number={number} />);
+            pieces.forEach(p => {
+                if(p.x === i && p.y === j) {
+                    image = p.image;
+                }
+            })
+
+            board.push(<Tile image={image} number={number} />);
         }
     }
     return <div id="chessboard">{board}</div>
